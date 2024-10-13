f878da0dd03ae35fa8635367d9611188eb44555b
haturatu <taro@eyes4you.org>
2024-10-14 05:11:14 +0900

    first commit

---
+++ xxx_backup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+MOUNT_DIR="/your/mount/point"
+
+SRC_DIR="/want/to/backup/dir"
+BK_DIR="backupdir"
+
+EXCLUDE_FILE=""  # 除外ファイル指定 --exclude=your/path
+W_DIR=`echo $SRC_DIR | awk -F/ '{print $(NF)}'`
+
+# マウントポイントを確認し、マウントされていなければマウントする
+check_mount() {
+    df | grep "$MOUNT_DIR" > /dev/null
+    if [ $? -ne 0 ]; then
+        break
+    else
+        mount $MOUNT_DIR || exit 1
+    fi
+}
+
+# 古いバックアップファイルを削除する
+rm_old_backups() {
+    BK_COUNT=`ls -1 $MOUNT_DIR/$BK_DIR/*.tar.gz 2>/dev/null | wc -l`
+    if [ "$BK_COUNT" -ge 3 ]; then
+        ls -1t $MOUNT_DIR/$BK_DIR/*.tar.gz | tail -n +4 | while read file; do
+            rm -f "$file"
+        done
+    fi
+}
+
+# バックアップを作成する
+create_backup() {
+    rsync -av $EXCLUDE_FILE $SRC_DIR $MOUNT_DIR/$BK_DIR
+    tar cfz $MOUNT_DIR/$BK_DIR/"$W_DIR"_`date +"%Y%m%d"`.tar.gz -C $MOUNT_DIR/$BK_DIR $W_DIR
+    rm -rf $MOUNT_DIR/$BK_DIR/$W_DIR/*
+}
+
+main() {
+    check_mount  # マウントを確認し、必要ならマウント
+    mkdir -p $MOUNT_DIR/$BK_DIR  # バックアップディレクトリの作成
+    rm_old_backups  # 古いバックアップの削除
+    create_backup  # 新しいバックアップの作成
+    umount $MOUNT_DIR || exit 1  # マウント解除
+}
+
+main
